aba8cb106b8ce4cbed40e040fab3f8aef24c6c8a
w014278e@student.staffs.ac.uk <w014278e@student.staffs.ac.uk>
2017-04-11 11:59:41 +0100

    offline service worker added
    offline service worker added

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,7 @@
 var BASE_PATH = '/homedemo2/';
 var CACHE_NAME = 'gih-cache-v9';
 var TEMP_IMAGE_CACHE_NAME = 'temp-cache-v1';
-var OFFLINE_URL = 'offline.html';
+
 
 
 
@@ -58,69 +58,32 @@ BASE_PATH + 'events.json',
 
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
+	var offlineRequest = new Request('offline.html');
   event.waitUntil(
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
-    // the actual URL we end up requesting might include a cache-busting parameter.
-    fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(response) {
-      return caches.open(CURRENT_CACHES.offline).then(function(cache) {
-        return cache.put(OFFLINE_URL, response);
+    fetch(offlineRequest).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        console.log('[oninstall] Cached offline page', response.url);
+        return cache.put(offlineRequest, response);
       });
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
-  let expectedCacheNames = Object.keys(CACHED_URLS).map(function(key) {
-    return CACHED_URLS[key];
-  });
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (expectedCacheNames.indexOf(cacheName) === -1) {
-            // If this cache name isn't present in the array of "expected" cache names,
-            // then delete it.
-            console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  // request.mode of 'navigate' is unfortunately not supported in Chrome
-  // versions older than 49, so we need to include a less precise fallback,
-  // which checks for a GET request with an Accept: text/html header.
-  if (event.request.mode === 'navigate' ||
-      (event.request.method === 'GET' &&
-       event.request.headers.get('accept').includes('text/html'))) {
-    console.log('Handling fetch event for', event.request.url);
-    event.respondWith(
-      fetch(event.request).catch(error => {
-        // The catch is only triggered if fetch() throws an exception, which will most likely
-        // happen due to the server being unreachable.
-        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-        // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-        // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-        console.log('Fetch failed; returning offline page instead.', error);
-        return caches.match(OFFLINE_URL);
+self.addEventListener('fetch', function(event) {
+	var request = event.request;
+	if (request.method === 'GET') {
+		event.respondWith(
+      fetch(request).catch(function(error) {
+		  console.error(
+          '[onfetch] Failed. Serving cached offline fallback ' +
+          error
+        );
+        return caches.open('offline').then(function(cache) {
+          return cache.match('offline.html');
+        });
       })
     );
   }
-
-  // If our if() condition is false, then this fetch handler won't intercept the request.
-  // If there are any other fetch handlers registered, they will get a chance to call
-  // event.respondWith(). If no fetch handlers call event.respondWith(), the request will be
-  // handled by the browser as if there were no service worker involvement.
-});
-
+  });
 
