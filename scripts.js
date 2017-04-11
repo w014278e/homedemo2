805c3bc62652b4010269632454636cadaa6818ba
w014278e@student.staffs.ac.uk <w014278e@student.staffs.ac.uk>
2017-04-11 13:33:23 +0100

    added localstorage to feedback page
    added localstorage to feedback page

--- scripts.js
+++ scripts.js
@@ -6,3 +6,31 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker registration failed: ', err);
   });
 }
+
+const feedbackForm = document.getElementById("feedbackForm");
+if(feedbackForm){
+    const feedbackFormSubmit = e => {
+        e.preventDefault();
+        const contact = {
+            "name": e.target.name.value,
+            "email": e.target.email.value,
+            "question": e.target.question.value,
+        };
+
+        fetch("https://w014278e.github.io/homedemo2/feedback.html", {mode: "no-cors", method: "POST", body: contact})
+            .then(response => {
+                console.log('Responses', response);
+            }).catch(() => {
+                let allContacts = [];
+
+                const existingContacts = localStorage.getItem('contact');
+                if(existingContacts){
+                    allContacts = JSON.parse(existingContacts);
+                }
+
+                allContacts.push(contact);
+                localStorage.setItem('contact', JSON.stringify(allContacts));
+            });
+    };
+    feedbackForm.addEventListener('submit', feedbackFormSubmit, false);
+}
